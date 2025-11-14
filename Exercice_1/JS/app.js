46ff8c98c9f2b57205e57d564d04017bbb817021
ireneeparfait <parfait0802@gmail.com>
2025-11-13 22:11:43 -0500

    Debut de code pour la construction du tableau

--- Exercice_1/JS/app.js
+++ Exercice_1/JS/app.js
@@ -17,12 +17,47 @@ function initialiser () {
   main.appendChild(compteur)
 
   document.body.appendChild(main)
-  
+
 fetch('http://localhost:8080/params_grille_nombres.php')
     .then(response => response.json())
     .then(data => {
       const config = data.parametres_tableau
       construireGrille(config)
+    
+      .catch(console.error)
     }) 
 }
 
+ function construireGrille (config) {
+  const nbLignes = config.nombre_lignes
+  const nbColonnes = config.nombre_colonnes
+  const min = config.plage_nombres_aleatoires.nombre_minimum
+  const max = config.plage_nombres_aleatoires.nombre_maximum
+  const seuil = config.nombre_base
+  const couleurFond = config.couleur_fond_cellule
+  const couleurTexte = config.couleur_texte_cellule
+ 
+  const container = document.getElementById('grille-container')
+  const compteurElm = document.getElementById('compteur')
+ 
+  const table = document.createElement('table')
+  let nbSupSeuil = 0
+ 
+  for (let i = 0; i < nbLignes; i++) {
+    const tr = document.createElement('tr')
+ 
+    for (let j = 0; j < nbColonnes; j++) {
+      const td = document.createElement('td')
+      const valeur = _.random(min, max)
+ 
+      td.textContent = valeur
+ 
+      if (valeur > seuil) {
+        nbSupSeuil++
+        td.style.backgroundColor = couleurFond
+        td.style.color = couleurTexte
+      }
+ 
+    }
+}
+ }
