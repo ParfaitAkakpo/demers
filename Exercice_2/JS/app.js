58523899e1e88a71f920af8fe42b32a140f0d9e4
ireneeparfait <parfait0802@gmail.com>
2025-11-30 01:32:22 -0500

    Fonction charger definitions

--- Exercice_2/JS/app.js
+++ Exercice_2/JS/app.js
@@ -7,3 +7,27 @@ window.addEventListener('DOMContentLoaded', () => {
   chargerDefinitionForces()
   initialiserFormulaire()
 })
+
+/* --------------------------------------------------------------
+   CHARGEMENT DÉFINITIONS
+-------------------------------------------------------------- */
+function chargerDefinitionForces () {
+  fetch('http://localhost:8080/definition_forces.php')
+    .then(res => res.json())
+    .then(data => {
+      const defs = data.definition_forces
+      const zone = document.getElementById('definition-forces')
+
+      let html = ''
+      defs.forEach(d => {
+        html += `<h5>${d.force}</h5>`
+        html += `<p>${d.definition}</p>`
+        html += '<hr>'
+      })
+      zone.innerHTML = html
+    })
+    .catch(() => {
+      document.getElementById('definition-forces').innerHTML =
+        '<p class="text-danger">Erreur de chargement des définitions.</p>'
+    })
+}
